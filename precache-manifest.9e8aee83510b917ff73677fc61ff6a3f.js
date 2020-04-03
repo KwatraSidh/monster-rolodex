cfa7115632d1b3f86e92dd820b2619d82d885e4a
Siddhartha <kwatra22siddhartha@gmail.com>
2020-04-03 14:20:20 +0530

    Updates

---
+++ precache-manifest.9e8aee83510b917ff73677fc61ff6a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42104ad273cdc64f7d2eeaa0b6edfd2a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "225ab459f5cef7885409",
+    "url": "/monster-rolodex/static/css/main.c07aadfa.chunk.css"
+  },
+  {
+    "revision": "cfceac3283c0ca996338",
+    "url": "/monster-rolodex/static/js/2.9c424b6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9c424b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225ab459f5cef7885409",
+    "url": "/monster-rolodex/static/js/main.96a7fb1b.chunk.js"
+  },
+  {
+    "revision": "bfd22c638cbcd931c63c",
+    "url": "/monster-rolodex/static/js/runtime-main.0bb500a1.js"
+  }
+]);
